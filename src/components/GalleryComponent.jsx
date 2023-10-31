f105e23ef7a5d4d1cf180a047c2d6c72d1fd0a6a
Roberto Aceves <roberto24aceves@outlook.es>
2023-10-31 08:42:57 -0600

    update gallery

--- src/components/GalleryComponent.jsx
+++ src/components/GalleryComponent.jsx
@@ -23,19 +23,26 @@ const GalleryComponent = () =>{
         {"title":"Proyecto 18"}];
     const indicator = useRef(null);
     const container = useRef(null);
+    const image = useRef(null);
     const handleMoveIndicator = (e) =>{
         let content = container.current;
         let ind = indicator.current;
         ind.style.left = `${e.clientX - content.getBoundingClientRect().left}px`;
     }
+    const handleHoverImage = (e)=>{
+        image.current.src = e.target.src;
+    }
     return (
         <div className="relative m-0 p-10 box-border w-full h-[600px]">
-            <div ref={container} className="container-gallery" onMouseMove={handleMoveIndicator}>
+            <div ref={container} className="container-gallery flex-col" onMouseMove={handleMoveIndicator}>
                 <div ref={indicator} className="indicator-gallery"></div>
+                <div className="md:hidden">
+                    <img ref={image} src="" className="w-full my-2 rounded" alt=""/>
+                </div>
                 <div className="gallery-content">
                     {
                         items.map((item, index) => (
-                            <div key={index} className={`${index >= 6 ? 'hidden md:block':''} gallery-item hover:w-full md:hover:w-96 hover:my-2`}>
+                            <div onMouseEnter={handleHoverImage} key={index} className={`${index >= 6 ? 'hidden md:block':''} gallery-item hover:opacity-70 md:hover:w-96`}>
                                 <img className="" src={idefault} alt=""/>
                             </div>
                         ))
