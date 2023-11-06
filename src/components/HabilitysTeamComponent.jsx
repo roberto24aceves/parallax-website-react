2c231a703bcb29f8af629b1f7a01d5d2ae39adeb
Roberto Aceves <roberto24aceves@outlook.es>
2023-11-06 10:16:51 -0600

    Corrections

--- src/components/HabilitysTeamComponent.jsx
+++ src/components/HabilitysTeamComponent.jsx
@@ -1,11 +1,11 @@
 const HabilitysTeamComponent = ({ habilitys }) => {
     return (
-        <div className="bg-pllx-black py-5 px-10 rounded-xl w-full flex flex-wrap items-center justify-between">
-            <div className="w-full md:w-1/2">
+        <div className="bg-pllx-black py-8 px-5 rounded-xl w-full flex flex-wrap items-center justify-between">
+            <div className="w-full md:w-1/2 lg:w-2/3 md:pr-4 lg:pr-8">
                 <h2 className="font-clash text-2xl md:text-4xl font-semibold text-white">Nuestra Experiencia</h2>
                 <p className="font-switzer text-white my-2">Cada persona en este equipo se ha especialidado en diferentes habilidades unicas, nosotros tuvimos la dicha de conocernos y crear algo unico, con estilo y escencia.</p>
             </div>
-            <div className="w-full md:w-1/2">
+            <div className="w-full md:w-1/2 lg:w-1/3">
                 <h3 className="font-clash font-semibold text-white">Capacidades</h3>
                 <div className=" flex flex-wrap gap-3 mt-2">
                     {
--- src/components/ServicesItemComponent.jsx
+++ src/components/ServicesItemComponent.jsx
@@ -1,21 +1,23 @@
+import {motion} from "framer-motion";
+
 const ServicesItemComponent = () =>{
     return(
-        <div className="flex flex-col gap-3 md:gap-0 md:flex-row bg-pllx-blue hover:bg-pllx-black group flex px-10 py-8 rounded-xl transition-all duration-300">
-            <div className="flex flex-col gap-2 w-full md:w-1/2">
-                <h3 className="font-clash font-semibold text-2xl md:text-4xl text-white">Servicios para Webs</h3>
-                <p className="font-switzer text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, doloribus eius, eligendi eum incidunt ipsam maiores neque nobis odit possimus soluta suscipit. Fugit, itaque numquam?</p>
-                <a className="btn w-fit bg-gradient-to-tl from-pllx-white to-white font-clash font-semibold text-pllx-blue group-hover:text-pllx-gray-dark" href="#">Chateemos!</a>
+        <motion.div whileHover={{scale:1.025,rotate:1}} className="flex flex-col gap-3 md:gap-0 md:flex-row bg-pllx-blue hover:drop-shadow-pllx-blue hover:bg-pllx-white group flex px-5 py-8 rounded-xl">
+            <div className="flex flex-col gap-2 w-full md:w-1/2 lg:w-2/3 md:pr-4 lg-pr-8  text-sm">
+                <h3 className="group-hover:text-pllx-black font-clash font-semibold text-2xl md:text-4xl text-white">Servicios para Webs</h3>
+                <p className="group-hover:text-pllx-black font-switzer text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, doloribus eius, eligendi eum incidunt ipsam maiores neque nobis odit possimus soluta suscipit. Fugit, itaque numquam?</p>
+                <a className="group-hover:text-pllx-blue group-hover:drop-shadow-pllx-blue btn w-fit bg-gradient-to-tl from-pllx-white to-white font-clash font-semibold text-pllx-blue group-hover:text-pllx-gray-dark" href="#">Chateemos!</a>
             </div>
-            <div className="flex flex-col w-full md:w-1/2">
-                <h4 className="font-clash font-semibold text-xl md:text-2xl text-white">Nuestros servicios</h4>
+            <div className="flex flex-col w-full md:w-1/2 lg:w-1/3">
+                <h4 className="group-hover:text-pllx-black font-clash font-semibold text-xl md:text-2xl text-white">Nuestros servicios</h4>
                 <div className="flex flex-wrap gap-2 mt-2">
-                    <span className="inline-block bg-pllx-white px-3 py-1 rounded-full text-pllx-blue font-semibold font-switzer group-hover:text-pllx-gray-dark">Diseño UX/UI</span>
-                    <span className="inline-block bg-pllx-white px-3 py-1 rounded-full text-pllx-blue font-semibold font-switzer group-hover:text-pllx-gray-dark">Desarrollo Web</span>
-                    <span className="inline-block bg-pllx-white px-3 py-1 rounded-full text-pllx-blue font-semibold font-switzer group-hover:text-pllx-gray-dark">Estrategia SEO</span>
-                    <span className="inline-block bg-pllx-white px-3 py-1 rounded-full text-pllx-blue font-semibold font-switzer group-hover:text-pllx-gray-dark">Consultoria SEO</span>
+                    <span className="inline-block bg-pllx-white px-3 py-1 rounded-full text-pllx-blue font-semibold font-switzer group-hover:text-pllx-gray-dark text-sm">Diseño UX/UI</span>
+                    <span className="inline-block bg-pllx-white px-3 py-1 rounded-full text-pllx-blue font-semibold font-switzer group-hover:text-pllx-gray-dark text-sm">Desarrollo Web</span>
+                    <span className="inline-block bg-pllx-white px-3 py-1 rounded-full text-pllx-blue font-semibold font-switzer group-hover:text-pllx-gray-dark text-sm">Estrategia SEO</span>
+                    <span className="inline-block bg-pllx-white px-3 py-1 rounded-full text-pllx-blue font-semibold font-switzer group-hover:text-pllx-gray-dark text-sm">Consultoria SEO</span>
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
 export default ServicesItemComponent;
