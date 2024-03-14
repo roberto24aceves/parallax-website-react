404b2fe98ebcded386a4b8dafa7452019e3c9a29
Roberto Aceves <roberto24aceves@outlook.es>
2024-03-14 10:53:51 -0600

    import vercel analitycs

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,7 @@
 import HeaderLayout from "./HeaderLayout.jsx";
 import FooterLayout from "./FooterLayout.jsx";
 import CursorComponent from "../components/CursorComponent.jsx";
+import { Analytics } from '@vercel/analytics/react';
 
 const Layout = ({children }) => {
     return(
@@ -8,6 +9,7 @@ const Layout = ({children }) => {
             <HeaderLayout/>
                 <div className="container px-4 md:px-16 py-5">
                     {children}
+                    <Analytics />
                 </div>
             <FooterLayout/>
         </div>
