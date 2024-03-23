1a1c432a18aedbf041ff38f8a401d32895c00a54
Roberto Aceves <roberto24aceves@outlook.es>
2024-03-23 07:44:10 -0600

    updater FooterLayout
    Se ajustaron los espacios laterales

--- src/layout/FooterLayout.jsx
+++ src/layout/FooterLayout.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 const FooterLayout = () => {
     return (
         <footer className="container pt-10 pb-10 mb-0">
-            <div className="flex flex-col sm:flex-row gap-5 justify-center sm:justify-between items-center">
+            <div className="flex flex-col sm:flex-row gap-5 justify-center sm:justify-between items-center px-4 md:px-16">
                 <span className="text-white text-base text-center sm:text-start font-switzer">
                     © 2019 Todos los derechos reservados. Parallax Estudio de desarrollo web.
                 </span>
