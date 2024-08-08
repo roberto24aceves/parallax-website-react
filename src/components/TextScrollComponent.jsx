5b86360f09f40798cbc2416d1d6bdb933260b36a
Roberto Aceves <roberto24aceves@outlook.es>
2024-08-08 09:11:36 -0600

    update-text-scroll-component

--- src/components/TextScrollComponent.jsx
+++ src/components/TextScrollComponent.jsx
@@ -13,10 +13,14 @@ const TextScrollComponent = ({text}) =>{
             <div className="py-3 px-10 bg-pllx-gray-dark w-[4000px] h-fit border-t border-b border-pllx-t-30">
                 <motion.div style={{x:scrollY}} transition={{type:'spring'}} className="flex justify-end gap-5">
                     {elements().map((item,index)=>(
-                        <div key={index}>
-                            <div
-                                 className="min-w-fit text-pllx-gray-light font-clash font-bold uppercase text-center">{item}</div>
-                            <img src="https://yxkdixicvsbupkvzjozk.supabase.co/storage/v1/object/public/parallax_public/assets/pllx-cross.svg" alt=""/>
+                        <div key={index} className="flex flex-row">
+                            <div className="flex flex-row gap-5 items-center justify-center">
+                                <span
+                                    className="min-w-fit text-pllx-gray-light font-clash font-bold uppercase text-center">{item}</span>
+                                <img className="w-5 block me-4"
+                                    src="https://yxkdixicvsbupkvzjozk.supabase.co/storage/v1/object/public/parallax_public/assets/pllx-cross.svg"
+                                    alt=""/>
+                            </div>
                         </div>
                     ))}
                 </motion.div>
