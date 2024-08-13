3cb69dd88ad7ae88bf6eff6d6335061c61cafc3c
roberto.aceves <roberto.aceves@anahuac.mx>
2024-08-13 10:42:12 -0600

    update-animation-elements

--- src/components/GalleryPortafolio/GalleryPortafolioItemComponent.jsx
+++ src/components/GalleryPortafolio/GalleryPortafolioItemComponent.jsx
@@ -13,6 +13,13 @@ const GalleryPortafolioItemComponent = ({title,extract,img,url,user}) =>{
     }
     return (
         <motion.div whileHover={{scale: 1.05}}
+                    initial={{ opacity: 0, scale: 0.75 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{
+                        duration: 0.8,
+                        ease: [0, 0.71, 0.2, 1.01],
+                        type: "spring",
+                    }}
                     className="group w-full md:basis-1/2 lg:basis-1/3 py-4 md:pe-4 rounded-xl hover:drop-shadow-pllx-gray cursor-pointer">
             <img className="group-hover:grayscale-0 grayscale rounded-2xl min-h-[280px] max-h-[280px] object-cover w-full border border-pllx-t-20" src={!img ? images.permanex : img} alt=""/>
             <div className="py-4 flex flex-col gap-2">
