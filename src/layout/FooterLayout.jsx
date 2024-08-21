dac9060d3337ca59e78e1150f8cc2d0d0fa62cc3
roberto.aceves <roberto.aceves@anahuac.mx>
2024-08-21 11:08:19 -0600

    add-lazy-load-footer-header

--- src/layout/FooterLayout.jsx
+++ src/layout/FooterLayout.jsx
@@ -1,11 +1,12 @@
 import images from "../images.jsx";
+import LazyImage from "./LazyImg.jsx";
 const FooterLayout = () => {
     return (
         <footer className="container pt-10 pb-10 mb-0">
             <div
                 className="flex flex-col sm:flex-row gap-5 justify-center sm:justify-between items-center px-4 md:px-16">
                 <a href={"https://parallax.lat"}>
-                    <img src={images.logo_footer} className="w-48" alt=""/>
+                    <LazyImage src={images.logo_footer} className="w-48" alt=""/>
                 </a>
                 <span className="text-white text-base text-center sm:text-start font-switzer">
                     © 2024 Todos los derechos reservados.
--- src/layout/HeaderLayout.jsx
+++ src/layout/HeaderLayout.jsx
@@ -1,4 +1,5 @@
 import images from "../images.jsx";
+import LazyImage from "./LazyImg.jsx";
 const HeaderLayout = () => {
 
     return (
@@ -6,7 +7,7 @@ const HeaderLayout = () => {
             <header className="container py-5">
                 <div className="flex justify-between items-center px-4 md:px-16">
                     <a href={"https://parallax.lat"}>
-                        <img src={images.logo} className="w-40" alt=""/>
+                        <LazyImage src={images.logo} className="w-40" alt=""/>
                     </a>
                     <div className="flex flex-row">
                         <div className="hidden lg:flex bg-pllx-black rounded-full flex-row gap-10 py-3 px-10">
