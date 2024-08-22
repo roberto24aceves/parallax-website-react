7e9b049587d4c23a74f3f1ab065f84ee3075c0bb
roberto.aceves <roberto.aceves@anahuac.mx>
2024-08-22 12:52:36 -0600

    add-skeletons-for-lazy-load

--- src/layout/LazyImg.jsx
+++ src/layout/LazyImg.jsx
@@ -1,17 +1,34 @@
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 import { useInView } from 'react-intersection-observer';
-const LazyImg = ({ className, src, alt }) => {
+import { useState, useEffect } from 'react';
+const LazyImg = ({ className, src, alt, baseColor = "#1e1e1e",highlightColor="#383838" }) => {
     const { ref, inView } = useInView({
         triggerOnce: true,
         threshold: 0.1,
     });
-
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+        if (inView && src) {
+            console.log(inView);
+            const img = new Image();
+            img.src = src;
+            img.onload = () => setIsLoaded(true);
+        }
+    }, [inView,src]);
+    console.log(inView);
     return (
-        <img
-            className={className}
-            ref={ref}
-            src={inView ? src : ""}
-            alt={alt}
-        />
+        <div
+            ref={ref}>
+            {!isLoaded && <Skeleton baseColor={baseColor} highlightColor={highlightColor} className={className}/>}
+            {inView && isLoaded && (
+                <img
+                    className={className}
+                    src={inView ? src : ""}
+                    alt={alt}
+                />
+            )}
+        </div>
     );
 }
 export default LazyImg
